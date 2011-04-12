2d4c3a697af16d9bdd9b40592e91beee97b6b7a9
Andrew Brown <adb1413@rit.edu>
2011-04-12 19:34:57 -0400

    Added hello-world files

---
+++ src/hello-world.c
@@ -0,0 +1,19 @@
+/*
+ * hello-world.c
+ *
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#include "hello-world.h"
+
+/**
+ * Runs the program.
+ * 
+ * @param argc Number of command line arguments
+ * @param argv Command line argument values
+ * @return Error code of program
+ */
+int main(int argc, char *argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ src/hello-world.h
@@ -0,0 +1,12 @@
+/*
+ * hello-world.h
+ *
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#ifndef HELLO_WORLD_H_
+#define HELLO_WORLD_H_
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif /* HELLO_WORLD_H_ */
